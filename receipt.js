b2e8e0ee2cd3fa6f573ffb36336cde9788eb2c40
Patan08 <f4th4n081004@gmail.com>
2024-09-30 01:00:15 +0700

    Add files via upload

---
+++ receipt.js
@@ -0,0 +1,45 @@
+function showDetail1() {
+    const judul = document.querySelector('.judul')
+    judul.style.display = "none"
+    const boxdetail = document.querySelector('.box-detail1')
+    boxdetail.style.display = 'flex'
+    const detail = document.querySelector('.detail')
+    detail.style.display = 'flex'
+    const container = document.querySelector('.container')
+    container.style.display = 'none'
+}
+function showDetail2() {
+    const judul = document.querySelector('.judul')
+    judul.style.display = "none"
+    const boxdetail = document.querySelector('.box-detail2')
+    boxdetail.style.display = 'flex'
+    const detail = document.querySelector('.detail')
+    detail.style.display = 'flex'
+    const container = document.querySelector('.container')
+    container.style.display = 'none'
+}
+function showDetail3() {
+    const judul = document.querySelector('.judul')
+    judul.style.display = "none"
+    const boxdetail = document.querySelector('.box-detail3')
+    boxdetail.style.display = 'flex'
+    const detail = document.querySelector('.detail')
+    detail.style.display = 'flex'
+    const container = document.querySelector('.container')
+    container.style.display = 'none'
+}
+
+function hideDetail() {
+    const judul = document.querySelector('.judul')
+    judul.style.display = "flex"
+    const boxdetail1 = document.querySelector('.box-detail1')
+    boxdetail1.style.display = 'none'
+    const boxdetail2 = document.querySelector('.box-detail2')
+    boxdetail2.style.display = 'none'
+    const boxdetail3 = document.querySelector('.box-detail3')
+    boxdetail3.style.display = 'none'
+    const detail = document.querySelector('.detail')
+    detail.style.display = 'none'
+    const container = document.querySelector('.container')
+    container.style.display = 'flex'
+}
